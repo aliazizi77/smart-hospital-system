d5d7a6bc7d4551ccd4ff8e5de3ce9539ba9f0154
ali azizi <aliazizi3264@gmail.com>
2019-08-27 02:41:57 +0430

    add login page and logic

--- android/app/src/main/java/ir/yasg/smarthospital/MainActivity.java
+++ android/app/src/main/java/ir/yasg/smarthospital/MainActivity.java
@@ -2,13 +2,54 @@ package ir.yasg.smarthospital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    EditText userName;
+    EditText password;
+
+    String user;
+    String pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialization();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                user = userName.getText().toString();
+                pass = password.getText().toString();
+                Intent intent = new Intent(MainActivity.this , nurseDashboard.class);
+                startActivity(intent);
+                if (user == "docter" && pass == "123456"){
+                   // Intent intent = new Intent(MainActivity.this , nurseDashboard.class);
+                    //startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this, "عملیات ورود با مشکل مواجه شده است!!!", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
+
+
     }
+    public void initialization(){
+        btnLogin = findViewById(R.id.btn_login);
+        userName = findViewById(R.id.et_user_name);
+        password = findViewById(R.id.et_password);
+
+    }
+
 }
